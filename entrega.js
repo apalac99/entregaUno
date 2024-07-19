89f60f92481b465fdce182597e90962ae2258463
Alejandro Palacios <apalac99@gmail.com>
2024-07-19 10:37:29 -0400

    todo

--- entrega.js
+++ entrega.js
@@ -1,7 +1,35 @@
 /*Jose Palacios
 Pre-entrega 1 JS 
-Encuentra la culebra
+Encuentra el conejo
 */
 
+
+
+
+const campo = []; // de 0 a 50
+for (i = 0; i <= 50; i++) {
+  campo[i]=false;
+}
+
+function getRandomInt(max) {   // funcion que obtiene un numero aleatorio entre 0 y 50
+    return Math.floor(Math.random() * max);
+
+  }
+
+lugar = getRandomInt(50);
+campo[lugar]=true;
+console.table(campo);
+
+
+intentoNumero = 1;
+
+//alert("Bienvenido al juego de la culebra \nDebes descubrir donde esta el conejo"); 
+ 
+
+
+do {
+  intento = prompt("Intento", intentoNumero + " Escribe un numero del 0 al 50 donde creas que está el conejo");  // propmt
+  
+} while (  );
 
 
